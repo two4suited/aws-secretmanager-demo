77dd91b78ed596ed25cbdff7a92886fb4f4a72a6
Brian Sheridan <brian.sheridan@gmail.com>
2020-01-25 16:00:39 -0800

    Setup Function

--- src/RetrieveSecrets/Function.cs
+++ src/RetrieveSecrets/Function.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Amazon.Lambda.Core;
+using Microsoft.Extensions.DependencyInjection;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace RetrieveSecrets
 {
+    
+    
     public class Function
     {
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
+        private readonly IServiceProvider _serviceProvider;
+
+        public Function(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public Function() 
+        {
+            _serviceProvider = Startup.Container.BuildServiceProvider();
+
+        }
         public string FunctionHandler(string input, ILambdaContext context)
         {
             return input?.ToUpper();
---
+++ src/RetrieveSecrets/Startup.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace RetrieveSecrets
+{
+    public static class Startup
+    {
+        public static IServiceCollection Container => ConfigureServices(Configuration); 
+               
+        private static IConfigurationRoot Configuration => new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+        
+        private static IServiceCollection ConfigureServices(IConfigurationRoot root)
+        {
+            var services = new ServiceCollection();
+            
+            
+            
+            services.AddLogging(x =>
+            {
+                x.AddConsole();
+                x.AddAWSProvider();
+                x.SetMinimumLevel(LogLevel.Information);
+            });
+           
+            return services;
+        
+        }
+    }
+}
